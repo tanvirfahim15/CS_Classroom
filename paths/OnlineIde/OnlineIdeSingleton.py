dad1c1b063596f8a1f5b72d834950da97f9dbb26
mashrur29 <mashrur639@gmail.com>
2018-10-25 20:14:51 +0600

    Ide Singleton Semaphore Added

--- paths/OnlineIde/OnlineIdeSingleton.py
+++ paths/OnlineIde/OnlineIdeSingleton.py
@@ -1,11 +1,17 @@
+from threading import Lock
+
+
 class Singleton(type):
+    lock = Lock()
 
     def __init__(cls, name, bases, attrs, **kwargs):
         super().__init__(name, bases, attrs)
         cls._instance = None
 
     def __call__(cls, *args, **kwargs):
+        Singleton.lock.acquire()
         if cls._instance is None:
             cls._instance = super().__call__(*args, **kwargs)
+        Singleton.lock.release()
         return cls._instance
 
