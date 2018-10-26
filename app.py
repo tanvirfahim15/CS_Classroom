f2205127b6d4cf7a2e5f0db626cab5230090936d
mashrur29 <mashrur639@gmail.com>
2018-10-27 02:54:03 +0600

    Statistical Calculator Index Fixed

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ from paths.Simulation.LinearAlgebra.main import app as linear_algebra
 from paths.Simulation.DBMS.main import app as dbms
 from paths.StatisticalCalculator.DeviationCalculator import app as dev_calc
 from paths.StatisticalCalculator.FrequencyCalculator import app as freq_calc
+from paths.StatisticalCalculator.main import app as statis_calc
 
 app = Flask(__name__)
 app.secret_key = 'UIBBN*E(DNJ'
@@ -95,8 +96,10 @@ app.register_blueprint(classroom_feed)
 
 
 # Statistical Calculator
+app.register_blueprint(statis_calc)
 app.register_blueprint(freq_calc)
 app.register_blueprint(dev_calc)
+
 
 @app.route("/")
 def home():
