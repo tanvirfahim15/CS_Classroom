6647d70499421ff4647c4bccec94123dc4c18575
zaoad <zaoadcsedu22.23@gmail.com>
2018-11-10 02:42:47 +0600

    Answer

---
+++ classes/OnlineClassroom/Answer.py
@@ -0,0 +1,10 @@
+class Answer:
+    question=''
+    answer=''
+    def __init__(self,question,answer):
+        self.question=question
+        self.answer=answer
+    def get_question(self):
+        return self.question
+    def get_answer(self):
+        return self.answer
